6b157bd50d5b00b76b7ab0994242eddfce546b32
Cleo Nguyen <cleo.nguyen@gmail.com>
2025-01-16 12:21:12 +1100

    Explore various data types float, integer

--- variables.py
+++ variables.py
@@ -11,6 +11,12 @@ print(day)
 
 date = date.today() #Initially did not import date from datetime (required for date format)
 print(date)
-# Correct the date assignment
+
 date_today = date.fromisocalendar(2025,1,5).weekday()
 print(date_today)
+
+day = 21
+temp =-15
+weight = 190.468 #decimal called float (+/-)
+print(weight*2)
+
