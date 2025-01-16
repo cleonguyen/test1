706b15ecb8ddb3c457a16669b5330c790f78f8a4
Cleo Nguyen <cleo.nguyen@gmail.com>
2025-01-16 12:55:55 +1100

    adding f string, variables in text and using booleans in if statements

--- variables.py
+++ variables.py
@@ -29,3 +29,21 @@ print(shirt)
 store = "Nick's Pizza Shop, the 'best' there is" #if quoting can use \ to indicate e.g. "Nick\'s 
 store = 'Nick\'s Pizza Shop, the "best" there is' # or using apostrophes
 print(store)
+
+day = 16
+month = "January"
+temp = -15 
+
+print(f"Today is {month} {day} and it is {temp} degrees outside") #f-string at the front of text to insert variables into string
+
+light_is_on = True #boolean, doens't need quotes, because it will become a string (True or False only)
+if light_is_on:
+    print("The light is on!") #needs the tab or | to indicate it is part of the if statement
+else:
+    print("We're in the dark!") #if var light_is_on is False, will run else statement
+
+dad_weight = 76.5
+if dad_weight < 100: #condition if ____ : , == exactly != not equal to
+    print("You're not overweight, probably...")
+else:
+    print("You gotta exercise more!")
